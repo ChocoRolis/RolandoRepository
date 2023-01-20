3b12642c108aefb9e16534918514f492b8304b00
ChocoRolis <119354460+ChocoRolis@users.noreply.github.com>
2023-01-20 10:59:46 -0600

    Add files via upload

---
+++ Tarea1Obj.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int residuo(string Num) {
+    int s = 0, i, n;
+    for (i = 0; i<8; i++){
+        n = (int) Num[i];
+        n = n - 48;
+        s = s + (i * n);
+    }
+    n = s % 11;
+    return n;
+}
+
+int main(){
+    char Char = 'Y', h;
+    int r;
+    string num;
+    while (Char == 'Y'){
+        cout << "Teclea el numero de 9 digitos" << endl;
+        getline(cin, num);
+        r = residuo(num);
+        if (r < 10){
+            h = (char) r;
+            num += (h + 48);
+        }
+        else if (r > 10) {
+            num += "X";
+        }
+        cout << "Tu nuevo ISBN es " << num << endl;
+        cout << "Quieres repetir el proceso? (Y/n)";
+        cin >> Char;        
+    }
+}
